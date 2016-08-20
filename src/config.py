64c682b4c71a1a0ce5435983fd78e2268f1c8c68
Buğra İşgüzar <biisguzar@gmail.com>
2016-08-20 11:08:11 +0300

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,16 @@
+userID 		  = 'lolasistan1' #LoL account's username
+passw		  = 'bisguzar15'  #LoL account's password
+
+masterAccounts = ['Bisguzar', 'admin2', 'admin3', 'admin4'] #Enter here admin's username
+
+addCommPrefix = '!' #Dont change this if 
+					#you no have an idea
+					#PLEASE ENTER ONE CHARACTER!
+
+delCommand    = 'del'#Dont change this if 
+					 #you no have an idea
+
+msgCommand    = 'msg'#Dont change this if 
+					 #you no have an idea
+
+language	  = 'tr'#You may use en and tr yet.
